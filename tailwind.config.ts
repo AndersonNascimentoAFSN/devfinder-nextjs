29468a65bd6eb0fab6637d698af93be89350252d
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-10-03 12:04:02 -0300

    chore: add var colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,31 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: '#FFFFFF',
+        gray: {
+          25: '#FEFEFE',
+          50: '#F6F8FF',
+          600: '#697C9A',
+          800: '#2B3442',
+          900: '#222731',
+        },
+        cyan: {
+          200: '#90A4D4',
+          500: '#4B6A9B',
+          600: '#1E2A47',
+          800: '#141D2F',
+        },
+        blue: {
+          50: '#60ABFF',
+          150: '#0079FF',
+        },
+        error: {
+          50: '#F74646'
+        },
+      },
+    }
   },
   plugins: [],
 }
